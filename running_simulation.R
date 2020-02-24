964a369539bb8404c71fafb7f059cf6d0f6377c9
annariha <rihaanna@student.hu-berlin.de>
2020-02-24 14:38:28 +0100

    removed pipe operator as there might be conflicts btw pipe and bnnlib

--- running_simulation.R
+++ running_simulation.R
@@ -4,38 +4,40 @@
 args <- commandArgs(trailingOnly = TRUE)
 
 if(!requireNamespace("pacman"))install.packages("pacman")
-pacman::p_load(tictoc, tidyverse, gridExtra, furrr, fs, here)
+pacman::p_load(tictoc, tidyverse, gridExtra, fs, here)
 
 conditions <- read_rds(args[[1]])
 
 # setup for bnnlib 
-dyn.load(paste(here("bnnlib"), .Platform$dynlib.ext, sep=""))
+dyn.load(here("bnnlib", str_c("bnnlib", .Platform$dynlib.ext)))
 source(here("bnnlib", "bnnlib.R"))
 cacheMetaData(1)
-dir_walk(here("bnnlib", "R"), source)
+# dir_walk(here("bnnlib", "R"), source)
+source(here("bnnlib", "R", "toSequence.R"))
 
 # load functions for simulation 
 source(here("gen_frequencies.R"))
 source(here("get_errors.R"))
 
 #----workhorse----
+conditions <- sample_n(conditions, 2)
+
 tic()
-results <- 
-  conditions %>% 
-  sample_n(4) %>% 
-  mutate(.,
-    values_error = pmap(., get_errors)
-    # values_error = future_pmap(., get_errors)
-  )
+results <- pmap(conditions, get_errors)
 toc()
+
+results <- 
+  mutate(conditions,
+    values_error = results
+  )
 
 # unnest results to get df matching conditions and 
 # trainerrors for all iterations and models (800x500) 
 # and testerrors for all 800 models
-results <- results %>% mutate(testerror = map_dbl(values_error, "testerror"),
-                   trainerror = map(values_error, "trainerror")) %>% 
-  select(-values_error) %>% 
-  unnest(cols = trainerror)
+results$testerror <- map_dbl(results$values_error, "testerror")
+results$trainerror <- lapply(map(results$values_error, "trainerror"), as.data.frame)
+results$values_error <- NULL
+results <- unnest(results, cols = trainerror)
 
 #----write-simulation----
 dir_create(path_dir(args[[2]]))
